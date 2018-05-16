08e02ee1b67a034ffdaa2f97374cc85484bb7f84
NascaSergiu <sergiu1_nasca@yahoo.com>
2018-05-16 12:38:41 +0300

    update

--- client.py
+++ client.py
@@ -1,9 +1,9 @@
 import bluelet
 
 def connecho():
-    c = yield bluelet.connect("127.0.0.1", 8000)
+    c = yield bluelet.connect("192.168.1.10", 8000)
     while True:
-        yield c.send("Ana are mere".encode())
+        yield c.send("Diana are pofte".encode())
         data = yield c.recv(1024)
         print("SERVER:", data)
         #raw_input("HEY")
--- server.py
+++ server.py
@@ -1,9 +1,23 @@
 import socket
 
+import fcntl
+import struct
+
+
+def get_ip_address(ifname):
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    return socket.inet_ntoa(fcntl.ioctl(
+        s.fileno(),
+        0x8915,  # SIOCGIFADDR
+        struct.pack('256s', ifname[:15])
+    )[20:24])
+
+
 HOST = ""
 PORT = 8000
 
 print("Waiting clients . . .")
+
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((HOST, PORT))
 s.listen(1)
@@ -11,7 +25,9 @@ conn, addr = s.accept()
 print("Connected by", addr)
 while True:
     data = conn.recv(1024)
-    if not data: break
+    if not data:
+        break
+    print(data)
     conn.sendall(data)
 conn.close()
 
